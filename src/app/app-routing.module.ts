34b701a349a1904aa9092142141c5fcf1345b24b
ThutaAung999 <exercise999123@gmail.com>
2023-07-17 11:54:54 +0630

    before Home page featured area design

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,10 +6,11 @@ import { SinglePostComponent } from './pages/single-post/single-post.component';
 import { AboutUsComponent } from './pages/about-us/about-us.component';
 import { TermsAndConditionComponent } from './pages/terms-and-condition/terms-and-condition.component';
 import { ContactUsComponent } from './pages/contact-us/contact-us.component';
+import { SingleCategoryComponent } from './pages/single-category/single-category.component';
 
 const routes:Routes=[  
   {path:'',component:HomeComponent},
-  {path:'category', component:CategoryNavbarComponent},
+  {path:'category', component:SingleCategoryComponent},
   {path:'post', component:SinglePostComponent},
 
 
